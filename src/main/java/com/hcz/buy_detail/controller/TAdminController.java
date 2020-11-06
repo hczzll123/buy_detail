0974d36237cb98847cd040b98630ed39191cba5d
hechangzheng <hcz_mail@163.com>
2020-11-06 15:29:09 +0800

    no message

--- src/main/java/com/hcz/buy_detail/controller/TAdminController.java
+++ src/main/java/com/hcz/buy_detail/controller/TAdminController.java
@@ -1,8 +1,13 @@
 package com.hcz.buy_detail.controller;
 
 
+
+import com.hcz.buy_detail.entity.TAdmin;
+import com.hcz.buy_detail.service.TAdminService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -16,6 +21,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/t-admin")
 public class TAdminController {
+
+    @Autowired
+    TAdminService tAdminService;
+
+    @RequestMapping("add")
+    @ResponseBody
+    public String addAdmin(){
+
+        TAdmin tAdmin = new TAdmin();
+        tAdmin.setLoginAcct("hcz").setUserPswd("Aa888888").setUserName("何昌政").setEmail("hcz@qq.com").setCreateTime("20200414");
+        tAdminService.save(tAdmin);
+
+        return "成功";
+    }
+
+
 
 }
 
