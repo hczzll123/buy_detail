92c0cbbe37128272cf38e79370d6f42afcbc94f6
hechangzheng <943587235@qq.com>
2023-04-23 14:48:51 +0800

    update: 精简版本

--- src/main/java/com/hcz/buy_detail/BuyDetailApplication.java
+++ src/main/java/com/hcz/buy_detail/BuyDetailApplication.java
@@ -1,12 +1,10 @@
 package com.hcz.buy_detail;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@MapperScan("com.hcz.buy_detail.mapper")
 @EnableScheduling
 public class BuyDetailApplication {
 
--- src/main/java/com/hcz/buy_detail/controller/TAdminController.java
+++ src/main/java/com/hcz/buy_detail/controller/TAdminController.java
@@ -3,9 +3,6 @@ package com.hcz.buy_detail.controller;
 
 
 import com.hcz.buy_detail.config.RetVal;
-import com.hcz.buy_detail.entity.TAdmin;
-import com.hcz.buy_detail.redis.RedisUtil;
-import com.hcz.buy_detail.service.TAdminService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,34 +26,29 @@ import java.util.Map;
 @RequestMapping("/t-admin")
 public class TAdminController {
 
-    @Autowired
-    TAdminService tAdminService;
-
-    @Autowired
-    RedisUtil redisUtil;
 
     @GetMapping("/welcome")
     public RetVal welcome(){
 
-        String myName = (String) redisUtil.get("myName");
-        return RetVal.successWithData("查询成功",myName);
+//        String myName = (String) redisUtil.get("myName");
+        return RetVal.successWithData("查询成功",null);
     }
 
     @RequestMapping("add")
     @ResponseBody
     public RetVal addAdmin(){
 
-        List<TAdmin> list = tAdminService.list(null);
+//        List<TAdmin> list = tAdminService.list(null);
 
-        for (TAdmin tAdmin1 : list) {
-            String createTime = tAdmin1.getCreateTime();
-            System.out.println(createTime);
-        }
-        Map<String, Object> map = new HashMap<>();
-        map.put("name","hcz");
-        map.put("list",list);
+//        for (TAdmin tAdmin1 : list) {
+//            String createTime = tAdmin1.getCreateTime();
+//            System.out.println(createTime);
+//        }
+//        Map<String, Object> map = new HashMap<>();
+//        map.put("name","hcz");
+//        map.put("list",list);
 
-        return RetVal.successWithData("成功",list);
+        return RetVal.successWithData("成功",null);
     }
 
 
